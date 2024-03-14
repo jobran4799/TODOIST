e8427002ac2568ba140b76b0c9f1b9741c311481
jobran4799 <jobran4799@gmail.com>
2024-03-14 12:50:39 +0200

    new update

--- api_serial_runer.py
+++ api_serial_runer.py
@@ -4,7 +4,7 @@ import os
 from os.path import dirname as up
 import unittest
 from infra.UI.Brawser_Wrapper import BrowserWrapper
-from api_parallel_tests import api_parallel_tests
+from tests.API.api_parallel_tests import api_parallel_tests
 
 # try:
 #     with open('../../config.json') as f:
