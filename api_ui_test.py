697aa8b29eba4b4c85d3e275b5f424f7faa5768b
jobran4799 <jobran4799@gmail.com>
2024-03-19 13:13:20 +0200

    test ui api on gitaction

---
+++ api_ui_test.py
@@ -0,0 +1,32 @@
+import json
+import unittest
+
+from infra.UI.Brawser_Wrapper import BrowserWrapper
+from api_parallel_tests import api_parallel_tests
+
+list_test_cases_runer = [api_parallel_tests]
+# test_project_api, test_ui_project]
+
+def test_brawser_runer(browser):
+    for test_cases in list_test_cases_runer:
+        test_cases.browser = browser
+        test_suite = unittest.TestLoader().loadTestsFromTestCase(test_cases)
+        print(test_suite, browser)
+        unittest.TextTestRunner().run(test_suite)
+
+
+if __name__ == "__main__":
+    browser_wrapper = BrowserWrapper()
+    # cur_dir = os.path.abspath(__file__)
+    # config_location = os.path.join(cur_dir, 'config.json')
+    with open('config.json') as f:
+        data = json.load(f)
+    parallel = data["parallel"]
+    serial = data["serial"]
+    get_browser = data["browser"]
+    browsers = data["browser_types"]
+    if serial:
+        for browser in browsers:
+            test_brawser_runer(browser)
+    else:
+        test_brawser_runer(get_browser)
