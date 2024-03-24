6e32ac3b6509c610d5dd27595cf96fcf8568727f
jobran4799 <jobran4799@gmail.com>
2024-03-24 11:14:13 +0200

    fix test path

--- tests/UI/Main_Page_Test.py
+++ tests/UI/Main_Page_Test.py
@@ -13,7 +13,7 @@ class Main_page_test(unittest.TestCase):
     ISDELETED = False
     def setUp(self):
         self.browser_wrapper = BrowserWrapper()
-        self.driver = self.browser_wrapper.get_driver(self.browser)
+        self.driver = self.browser_wrapper.get_driver('chrome')
         self.my_api = APIWrapper()
         self.test_p = Tasks(self.my_api)
         login = LoginPage(self.driver)
