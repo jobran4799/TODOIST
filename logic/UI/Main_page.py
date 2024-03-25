9684dd9d41132d403b0fbdd4831a0f08e47ba81d
jobran4799 <jobran4799@gmail.com>
2024-03-25 13:34:21 +0200

    use webdriver wait for locate xpathes

--- logic/UI/Main_page.py
+++ logic/UI/Main_page.py
@@ -25,10 +25,10 @@ class MainPage(BasePage):
         return self.user_name.is_displayed()
 
     def wait_to_locate_path(self, xpath):
-        return (WebDriverWait(self._driver, 23).until(EC.element_to_be_clickable((By.XPATH, xpath))))
+        return (WebDriverWait(self._driver, 24).until(EC.element_to_be_clickable((By.XPATH, xpath))))
 
     def wait_path_to_be_clickbale(self, xpath):
-        return (WebDriverWait(self._driver, 23).until(EC.presence_of_element_located((By.XPATH, xpath))))
+        return (WebDriverWait(self._driver, 24).until(EC.presence_of_element_located((By.XPATH, xpath))))
     def fill_input(self, element, text):
         WebDriverWait(self._driver, 10).until(EC.element_to_be_clickable(element)).send_keys(text)
 
