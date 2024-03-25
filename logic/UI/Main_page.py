510df01ca817f3242df7210dc756cf27e312ea36
jobran4799 <jobran4799@gmail.com>
2024-03-25 20:26:33 +0200

    run 3 "priority" tests only

--- logic/UI/Main_page.py
+++ logic/UI/Main_page.py
@@ -166,7 +166,7 @@ class MainPage(BasePage):
 
     def find_menu_priority(self, task_name):
         xpath = f"//li[./div[./div[./div[./div[./div[./div[./div[contains(text(),'{task_name}')]]]]]]]]//button[contains(@aria-label,'More task actions')]"
-        self.menu_priority = self.wait_path_to_be_clickbale(23, xpath)
+        self.menu_priority = self.wait_path_to_be_clickbale(30, xpath)
 
     def find_task_for_priority(self, task_name):
         self.task_for_priority = self.wait_to_locate_path(10, f"//li[./div[./div[./div[./div[./div[./div[./div[contains(text(),'{task_name}')]]]]]]]]")
