358442646611a844b437f08389c4ef46cd267d03
jobran4799 <jobran4799@gmail.com>
2024-03-25 04:33:23 +0200

    test confyest file

--- Main_page_pytest.py
+++ Main_page_pytest.py
@@ -16,6 +16,12 @@ class TestMainPage:
         login = LoginPage(self.driver)
         login.fllow_log_in_test("beyonddevtestproject@gmail.com", "Zxcvbnm123")
 
+
+    def pre_teardown(self):
+        if self.TODELETED:
+            my_c_api = self.test_p.delete_tasks(self.ID)
+
+
     def teardown_method(self):
         if self.TODELETED:
             my_c_api = self.test_p.delete_tasks(self.ID)
@@ -31,6 +37,7 @@ class TestMainPage:
         for get_id in json_response:
             if get_id["content"] == task_name:
                 self.ID = get_id["id"]
+        self.pre_teardown()
 
     def test_task_priority(self):
         
@@ -41,6 +48,7 @@ class TestMainPage:
         self.ID = json_response["id"]
         main_page = MainPage(self.driver)
         main_page.priority_task(task_name, '3')
+        self.pre_teardown()
 
     def test_task_deletion(self):
         
@@ -51,6 +59,7 @@ class TestMainPage:
         self.ID = json_response["id"]
         main_page = MainPage(self.driver)
         main_page.delete_task(task_name)
+        self.pre_teardown()
 
     def test_task_set_due_date(self):
         
@@ -61,6 +70,7 @@ class TestMainPage:
         self.ID = json_response["id"]
         main_page = MainPage(self.driver)
         main_page.set_due_date_task(task_name)
+        self.pre_teardown()
 
     def test_task_compilation(self):
         
@@ -71,6 +81,7 @@ class TestMainPage:
         self.ID = json_response["id"]
         main_page = MainPage(self.driver)
         main_page.click_completed_task(task_name)
+        self.pre_teardown()
 
     def test_task_editing(self):
         
@@ -81,6 +92,7 @@ class TestMainPage:
         self.ID = json_response["id"]
         main_page = MainPage(self.driver)
         main_page.edit_task(task_name)
+        self.pre_teardown()
 
 
 
