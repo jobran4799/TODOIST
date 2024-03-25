4cebdd898c292f1a8502342d3458d9206e6f7dc2
jobran4799 <jobran4799@gmail.com>
2024-03-25 21:01:59 +0200

    run 4 "delete" tests only

--- Main_page_pytest.py
+++ Main_page_pytest.py
@@ -50,16 +50,16 @@ class TestMainPage:
         main_page.priority_task(task_name, '3')
         self.pre_teardown()
 
-    def test_task_deletion(self):
-
-        task_name = Utiles.generate_random_string(5)
-        body = {"content": task_name, "due_string": "today at 12:00", "due_lang": "en", "priority": 4}
-        my_c_api = self.test_p.create_tasks(body)
-        json_response = my_c_api.json()
-        self.ID = json_response["id"]
-        main_page = MainPage(self.driver)
-        main_page.delete_task(task_name)
-        self.pre_teardown()
+    # def test_task_deletion(self):
+    #
+    #     task_name = Utiles.generate_random_string(5)
+    #     body = {"content": task_name, "due_string": "today at 12:00", "due_lang": "en", "priority": 4}
+    #     my_c_api = self.test_p.create_tasks(body)
+    #     json_response = my_c_api.json()
+    #     self.ID = json_response["id"]
+    #     main_page = MainPage(self.driver)
+    #     main_page.delete_task(task_name)
+    #     self.pre_teardown()
     #
     # def test_task_set_due_date(self):
     #
