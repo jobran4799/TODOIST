ebc6aa278e42e255fd19c509e532a73ece918b06
jobran4799 <jobran4799@gmail.com>
2024-03-26 11:10:01 +0200

    run pytest

--- Main_page_pytest.py
+++ Main_page_pytest.py
@@ -22,6 +22,16 @@ class TestMainPage:
     def pre_teardown(self):
         if self.TODELETED:
             my_c_api = self.test_p.delete_tasks(self.ID)
+        if hasattr(self, '_outcome') and self._outcome.result:
+            result = self._outcome.result
+            if result.errors or result.failures:
+                # If test fails, create a JIRA issue
+                test_method_name = self._testMethodName
+                error_message = ""
+                for test, traceback_text in result.errors + result.failures:
+                    error_message += f"Test: {test}\n"
+                    error_message += f"Error: {traceback_text}\n"
+                Utiles.create_jira_issue(f"{test_method_name} Test Failed", error_message)
 
     # def check_the_result_of_test(self):
     #     if hasattr(self, '_outcome') and self._outcome.result:
@@ -36,19 +46,6 @@ class TestMainPage:
     #             create_jira_issue(f"{test_method_name} Test Failed", error_message)
 
     def teardown_method(self):
-        if self.TODELETED:
-            my_c_api = self.test_p.delete_tasks(self.ID)
-
-        if hasattr(self, '_outcome') and self._outcome.result:
-            result = self._outcome.result
-            if result.errors or result.failures:
-                # If test fails, create a JIRA issue
-                test_method_name = self._testMethodName
-                error_message = ""
-                for test, traceback_text in result.errors + result.failures:
-                    error_message += f"Test: {test}\n"
-                    error_message += f"Error: {traceback_text}\n"
-                Utiles.create_jira_issue(f"{test_method_name} Test Failed", error_message)
         self.driver.quit()
 
     def test_task_creation(self):
